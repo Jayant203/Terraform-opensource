c64c4e98591b5007321f8c6b916112aacd817a36
Jayant203 <63440494+Jayant203@users.noreply.github.com>
2021-07-23 15:50:58 +0530

    Create drupalinstallation.sh

---
+++ drupalinstallation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install tasksel
+sudo tasksel install lamp-server
+sudo mysql -u root -padmin -e "CREATE DATABASE drupal CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+sudo mysql -u root -padmin -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON drupal.* TO ‘drupaluser’@’localhost’ IDENTIFIED BY 'root';"
+wget https://ftp.drupal.org/files/projects/drupal-8.6.2.tar.gz
+tar -zxf drupal-8.6.2.tar.gz
+sudo mkdir /var/www/html/drupal
+sudo cp -R drupal-8.6.2/* drupal-8.6.2/.htaccess /var/www/html/drupal
+sudo mkdir /var/www/html/drupal/sites/default/files
+sudo chown www-data:www-data /var/www/html/drupal/sites/default/files
+sudo cp /var/www/html/drupal/sites/default/default.settings.php /var/www/html/drupal/sites/default/settings.php
+sudo chown www-data:www-data /var/www/html/drupal/sites/default/settings.php
+sudo systemctl restart apache2
+sudo sed -i '172s/.*/        AllowOverride All/' /etc/apache2/apache2.conf
+sudo systemctl restart apache2
+
+
