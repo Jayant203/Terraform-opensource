3b19cb6e3c7f951076a9086a30d0bab113cd625d
Jayant203 <63440494+Jayant203@users.noreply.github.com>
2021-07-26 16:10:31 +0530

    Update drupalinstallation.sh

--- drupalinstallation.sh
+++ drupalinstallation.sh
@@ -2,14 +2,22 @@
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install tasksel -y 
-sudo tasksel install lamp-server 
-sudo mysql -u root -padmin -e "CREATE DATABASE drupal CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
-sudo mysql -u root -padmin -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON drupal.* TO ‘drupaluser’@’localhost’ IDENTIFIED BY 'root';"
-wget https://ftp.drupal.org/files/projects/drupal-9.2.0.tar.gz
-tar -zxf drupal-9.2.0.tar.gz
+sudo apt-get install tasksel -y
+sudo tasksel install lamp-server
+
+if [ -f /etc/init.d/mysql* ]
+then
+        echo "Already installed"
+else
+        sudo mysql -u root -padmin -e "CREATE DATABASE drupal CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+        sudo mysql -u root -padmin -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON drupal.* TO ‘drupaluser’@’localhost’ IDENTIFIED BY 'root';"
+fi
+
+wget https://ftp.drupal.org/files/projects/$1.tar.gz
+tar -zxf $1.tar.gz
 sudo mkdir /var/www/html/drupal
-sudo cp -R drupal-9.2.0/* drupal-9.2.0/.htaccess /var/www/html/drupal
+
+sudo cp -R $1/* $1/.htaccess /var/www/html/drupal
 sudo mkdir /var/www/html/drupal/sites/default/files
 sudo chown www-data:www-data /var/www/html/drupal/sites/default/files
 sudo cp /var/www/html/drupal/sites/default/default.settings.php /var/www/html/drupal/sites/default/settings.php
@@ -17,5 +25,3 @@ sudo chown www-data:www-data /var/www/html/drupal/sites/default/settings.php
 sudo systemctl restart apache2
 sudo sed -i '172s/.*/        AllowOverride All/' /etc/apache2/apache2.conf
 sudo systemctl restart apache2
-
-
