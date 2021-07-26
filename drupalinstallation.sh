aad38ddb769ceeec1f0f37e31d8e72ed625fc3d6
Jayant203 <63440494+Jayant203@users.noreply.github.com>
2021-07-26 14:21:56 +0530

    Update drupalinstallation.sh

--- drupalinstallation.sh
+++ drupalinstallation.sh
@@ -2,8 +2,8 @@
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
-DEBIAN_FRONTEND=noninteractive sudo apt-get install tasksel -y 
-DEBIAN_FRONTEND=noninteractive sudo tasksel install lamp-server 
+sudo apt-get install tasksel -y 
+sudo tasksel install lamp-server 
 sudo mysql -u root -padmin -e "CREATE DATABASE drupal CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
 sudo mysql -u root -padmin -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON drupal.* TO ‘drupaluser’@’localhost’ IDENTIFIED BY 'root';"
 wget https://ftp.drupal.org/files/projects/drupal-9.2.0.tar.gz
