b4b6bb217482c581cbfcecb2bbcf31a1fa34debe
Jayant203 <63440494+Jayant203@users.noreply.github.com>
2021-07-27 16:18:26 +0530

    Create completescript.sh

---
+++ completescript.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+sudo apt-get install -y -q
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install tasksel -y
+sudo tasksel install lamp-server
+
+
+echo "Enter input: "
+read input
+
+if [ $input -eq "drupal" ]
+then
+        if [ -f /etc/init.d/mysql* ]
+        then
+                echo "Already installed"
+        else
+                sudo mysql -u root -padmin -e "CREATE DATABASE drupal CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+                sudo mysql -u root -padmin -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON drupal.* TO ‘drupaluser’@’localhost’ IDENTIFIED BY 'root';"
+        fi
+
+        D9="drupal-9.2.2"
+        wget https://ftp.drupal.org/files/projects/$D9.tar.gz
+        tar -zxf $D9.tar.gz
+        sudo mkdir -p /var/www/html/drupal
+        sudo cp -R $D9/* $D9/.htaccess /var/www/html/drupal
+        sudo mkdir /var/www/html/drupal/sites/default/files
+        sudo chown www-data:www-data /var/www/html/drupal/sites/default/files
+        sudo cp /var/www/html/drupal/sites/default/default.settings.php /var/www/html/drupal/sites/default/settings.php
+        sudo chown www-data:www-data /var/www/html/drupal/sites/default/settings.php
+        sudo systemctl restart apache2
+        sudo sed -i '172s/.*/        AllowOverride All/' /etc/apache2/apache2.conf
+        sudo systemctl restart apache2
+
+else
+        if [ -f /etc/init.d/mysql* ]
+        then
+                echo "Already installed"
+        else
+                sudo mysql -u root -padmin -e "CREATE DATABASE wordpress CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+                sudo mysql -u root -padmin -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON wordpress.* TO ‘pressuser’@’localhost’ IDENTIFIED BY 'root';"
+        fi
+
+        sudo a2enmod rewrite
+        sudo systemctl restart apache2
+        sudo apt install zip
+       # sudo mkdir -p /var/www/html
+       # sudo mkdir -p /var/www/wordpress
+        curl --output /tmp/wordpress.zip https://wordpress.org/latest.zip
+        sudo rm -fr /var/www/html
+        sudo unzip /tmp/wordpress.zip -d /var/www/
+        sudo mv /var/www/wordpress/ /var/www/html
+        sudo chown -R www-data.www-data /var/www/html
+        sudo systemctl restart apache2
+fi
