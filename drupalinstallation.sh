86424566a359edbfb04c2c7581d929dbfdc7223f
Jayant203 <63440494+Jayant203@users.noreply.github.com>
2021-07-27 18:53:13 +0530

    Update drupalinstallation.sh

--- drupalinstallation.sh
+++ drupalinstallation.sh
@@ -1,38 +1,60 @@
 
+
 #!/bin/bash
-
-#export DEBIAN_FRONTEND=noninteractive
-
-
+#export input
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 sudo apt-get install -y -q
-sudo apt-get update -y
+sudo apt-get update
 sudo apt-get upgrade -y
-#sudo chmod 777 /var/cache/debconf/
-#sudo chmod 777 /var/cache/debconf/passwords.dat
-#sudo apt-get install dialog apt-utils -y
-#echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-#sudo apt-get install -y -q dialog
 sudo apt-get install tasksel -y
 sudo tasksel install lamp-server
 
-if [ -f /etc/init.d/mysql* ]
-then
-        echo "Already installed"
-else
-        sudo mysql -u root -padmin -e "CREATE DATABASE drupal CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
-        sudo mysql -u root -padmin -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON drupal.* TO ‘drupaluser’@’localhost’ IDENTIFIED BY 'root';"
-fi
-D9="drupal-9.2.2"
-wget https://ftp.drupal.org/files/projects/$D9.tar.gz
-tar -zxf $D9.tar.gz
-sudo mkdir /var/www/html/drupal
 
-sudo cp -R $D9/* $D9/.htaccess /var/www/html/drupal
-sudo mkdir /var/www/html/drupal/sites/default/files
-sudo chown www-data:www-data /var/www/html/drupal/sites/default/files
-sudo cp /var/www/html/drupal/sites/default/default.settings.php /var/www/html/drupal/sites/default/settings.php
-sudo chown www-data:www-data /var/www/html/drupal/sites/default/settings.php
-sudo systemctl restart apache2
-sudo sed -i '172s/.*/        AllowOverride All/' /etc/apache2/apache2.conf
-sudo systemctl restart apache2
+#echo "Enter input: "
+#read input
+
+if [ $input=="drupal" ]
+then
+        if [ -f /etc/init.d/mysql* ]
+        then
+                echo "Already installed"
+        else
+                sudo mysql -u root -padmin -e "CREATE DATABASE drupal CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+                sudo mysql -u root -padmin -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON drupal.* TO ‘drupaluser’@’localhost’ IDENTIFIED BY 'root';"
+        fi
+
+        D9="drupal-9.1.6"
+        wget https://ftp.drupal.org/files/projects/$D9.tar.gz
+        tar -zxf $D9.tar.gz
+        sudo mkdir -p /var/www/html/drupal
+        sudo cp -R $D9/* $D9/.htaccess /var/www/html/drupal
+        sudo mkdir /var/www/html/drupal/sites/default/files
+        sudo chown www-data:www-data /var/www/html/drupal/sites/default/files
+        sudo cp /var/www/html/drupal/sites/default/default.settings.php /var/www/html/drupal/sites/default/settings.php
+        sudo chown www-data:www-data /var/www/html/drupal/sites/default/settings.php
+        sudo systemctl restart apache2
+        sudo sed -i '172s/.*/        AllowOverride All/' /etc/apache2/apache2.conf
+        sudo systemctl restart apache2
+
+else
+        if [ -f /etc/init.d/mysql* ]
+        then
+                echo "Already installed"
+        else
+                sudo mysql -u root -padmin -e "CREATE DATABASE wordpress CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+                sudo mysql -u root -padmin -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON wordpress.* TO ‘pressuser’@’localhost’ IDENTIFIED BY 'root';"
+        fi
+
+        sudo a2enmod rewrite
+        sudo systemctl restart apache2
+        sudo apt install zip
+       # sudo mkdir -p /var/www/html
+       # sudo mkdir -p /var/www/wordpress
+        curl --output /tmp/wordpress.zip https://wordpress.org/latest.zip
+        sudo rm -fr /var/www/html
+        sudo unzip -o /tmp/wordpress.zip -d /var/www/
+        sudo mv /var/www/wordpress/ /var/www/html
+        sudo chown -R www-data.www-data /var/www/html
+        sudo systemctl restart apache2
+fi
+
