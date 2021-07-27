7c942203e73e9ebe8fd8c693d91eb58e77f685d1
Jayant203 <63440494+Jayant203@users.noreply.github.com>
2021-07-27 11:54:07 +0530

    Update drupalinstallation.sh

--- drupalinstallation.sh
+++ drupalinstallation.sh
@@ -1,7 +1,18 @@
+
 #!/bin/bash
 
+#export DEBIAN_FRONTEND=noninteractive
+
+
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+sudo apt-get install -y -q
 sudo apt-get update -y
 sudo apt-get upgrade -y
+#sudo chmod 777 /var/cache/debconf/
+#sudo chmod 777 /var/cache/debconf/passwords.dat
+#sudo apt-get install dialog apt-utils -y
+#echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+#sudo apt-get install -y -q dialog
 sudo apt-get install tasksel -y
 sudo tasksel install lamp-server
 
@@ -12,12 +23,12 @@ else
         sudo mysql -u root -padmin -e "CREATE DATABASE drupal CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
         sudo mysql -u root -padmin -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON drupal.* TO ‘drupaluser’@’localhost’ IDENTIFIED BY 'root';"
 fi
-
-wget https://ftp.drupal.org/files/projects/$1.tar.gz
-tar -zxf $1.tar.gz
+D9="drupal-9.2.2"
+wget https://ftp.drupal.org/files/projects/$D9.tar.gz
+tar -zxf $D9.tar.gz
 sudo mkdir /var/www/html/drupal
 
-sudo cp -R $1/* $1/.htaccess /var/www/html/drupal
+sudo cp -R $D9/* $D9/.htaccess /var/www/html/drupal
 sudo mkdir /var/www/html/drupal/sites/default/files
 sudo chown www-data:www-data /var/www/html/drupal/sites/default/files
 sudo cp /var/www/html/drupal/sites/default/default.settings.php /var/www/html/drupal/sites/default/settings.php
